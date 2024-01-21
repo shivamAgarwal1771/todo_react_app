83a7e404538002c607705b6c2d0fb2cf705a9184
Shivam Agarwal <Shivam220802@exlservice.com>
2024-01-21 20:38:11 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -4,16 +4,6 @@ import App from './App';
 import Home from './Home'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 
-const router =createBrowserRouter([
-  {
-    path:"/about",
-    element:<App/>
-  },
-  {
-    path:"/a",
-    element:<Home/>
-  }
-])
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
